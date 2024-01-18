91d973c8a044aa3f455c92fe11126eebbfd1f2a4
forestbaba <hawlahethan@gmail.com>
2024-01-18 21:24:04 +0100

    first commit

--- Program.cs
+++ Program.cs
@@ -1,17 +1,26 @@
 using API.Data;
 using Microsoft.EntityFrameworkCore;
+using API.Interfaces;
+using API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddScope<ITokenService, TokenService>();
+
 builder.Services.AddDbContext<DataContext>(opt => 
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https ://localhost:4200"));
 
 app.MapControllers();
 
